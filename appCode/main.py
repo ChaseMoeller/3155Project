9de503805fbabf886caa79275b7fc2be4e248676
Chase <cmoelle2@uncc.edu>
2021-11-23 16:58:10 -0500

    added basic register and login

--- appCode/main.py
+++ appCode/main.py
@@ -1,116 +1,147 @@
-# imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask   # Flask is the web app that we will customize
+import os
+import bcrypt
+from flask import Flask
 from flask import render_template
 from flask import request
-from flask import redirect, url_for
+from flask import redirect
+from flask import url_for
 from database import db
 from models import Post as Post
 from models import User as User
+from flask import session
+import datetime
+from forms import RegisterForm
+from forms import LoginForm
+from models import Comment as Comment
+from forms import CommentForm
 
-app = Flask(__name__)     # create an app
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
-#  Bind SQLAlchemy db object to this Flask app
+app.config['SECRET_KEY'] = 'SE3155'
 db.init_app(app)
-# Setup models
 with app.app_context():
-    db.create_all()   # run under the app context
+    db.create_all()
+
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user =  db.session.query(User).filter_by(email='placeholder@temp.com').first()
-
-    return render_template('index.html', user = a_user)
+    if session.get('user'):
+        return render_template("index.html", user=session['user'])
+    return render_template("index.html")
 
 @app.route('/posts')
 def get_posts():
-    a_user = db.session.query(User).filter_by(email='placeholder@temp.com').first()
-    my_posts = db.session.query(Post).all()
-
-    return render_template('posts.html', posts=my_posts, user=a_user)
+    if session.get('user'):
+        my_posts = db.session.query(Post).filter_by(user_id=session['user_id']).all()
+        return render_template('posts.html', posts=my_posts, user=session['user'])
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/posts/<post_id>')
 def get_post(post_id):
-    a_user = db.session.query(User).filter_by(email='placeholder@temp.com').first()
-    my_post = db.session.query(Post).filter_by(id=post_id).first()
-
-    return render_template('post.html', post=my_post, user=a_user)
+    if session.get('user'):
+        my_post = db.session.query(Post).filter_by(id=post_id, user_id=session['user_id']).one()
+        form = CommentForm()
+        return render_template('post.html', post=my_post, user=session['user'], form=form)
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/posts/new', methods=['GET', 'POST'])
 def new_post():
-    if request.method == 'POST':
-
-        title = request.form['title']
-        text = request.form['postText']
-        from datetime import date
-        today = date.today()
-        today = today.strftime("%m-%d-%Y")
-
-        new_record = Post(title, text, today)
-        db.session.add(new_record)
-        db.session.commit()
-
-        return redirect(url_for('get_posts'))
-
+    if session.get('user'):
+        if request.method == 'POST':
+            title = request.form['title']
+            text = request.form['postText']
+            from datetime import date
+            today = date.today()
+            today = today.strftime("%m-%d-%Y")
+            new_record = Post(title, text, today, session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+            return redirect(url_for('get_posts'))
+        else:
+            return render_template('new.html', user=session['user'])
     else:
-
-        a_user = db.session.query(User).filter_by(email='placeholder@temp.com').first()
-        return render_template('new.html', user=a_user)
-
+        return redirect(url_for('login'))
 
 @app.route('/posts/edit/<post_id>', methods=['GET', 'POST'])
 def update_post(post_id):
-
-    if request.method == 'POST':
-        title=request.form['title']
-        text=request.form['postText']
-        post =db.session.query(Post).filter_by(id=post_id).first()
-        post.title=title
-        post.text=text
-        db.session.add(post)
-        db.session.commit()
-        return redirect(url_for('get_posts'))
-
+    if session.get('user'):
+        if request.method == 'POST':
+            title = request.form['title']
+            text = request.form['postText']
+            post = db.session.query(Post).filter_by(id=post_id).one()
+            post.title = title
+            post.text = text
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('get_posts'))
+        else:
+            my_post = db.session.query(Post).filter_by(id=post_id).one()
+            return render_template('new.html', post=my_post, user=session['user'])
     else:
-        a_user = db.session.query(User).filter_by(email='placeholder@temp.com').first()
-        my_post =db.session.query(Post).filter_by(id=post_id).first()
-        return render_template('new.html', post=my_post, user=a_user)
-
+        return redirect(url_for('login'))
 
 @app.route('/posts/delete/<post_id>', methods=['POST'])
 def delete_post(post_id):
-
-    my_post =db.session.query(post).filter_by(id=post_id).first()
-    db.session.delete(my_post)
-    db.session.commit()
-    return redirect(url_for('get_posts'))
-
+    if session.get('user'):
+        my_post = db.session.query(Post).filter_by(id=post_id).one()
+        db.session.delete(my_post)
+        db.session.commit()
+        return redirect(url_for('get_posts'))
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegisterForm()
-
     if request.method == 'POST' and form.validate_on_submit():
-        # salt and hash password
         h_password = bcrypt.hashpw(
             request.form['password'].encode('utf-8'), bcrypt.gensalt())
-        # get entered user data
         first_name = request.form['firstname']
         last_name = request.form['lastname']
-        # create user model
         new_user = User(first_name, last_name, request.form['email'], h_password)
-        # add user to database and commit
         db.session.add(new_user)
         db.session.commit()
-        # save the user's name to the session
         session['user'] = first_name
-        session['user_id'] = new_user.id  # access id value from user model of this newly added user
-        # show user dashboard view
+        session['user_id'] = new_user.id
         return redirect(url_for('get_posts'))
-
-    # something went wrong - display register view
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        the_user = db.session.query(User).filter_by(email=request.form['email']).one()
+        if bcrypt.checkpw(request.form['password'].encode('utf-8'), the_user.password):
+            session['user'] = the_user.first_name
+            session['user_id'] = the_user.id
+            return redirect(url_for('get_posts'))
+        login_form.password.errors = ["Incorrect username or password."]
+        return render_template("login.html", form=login_form)
+    else:
+        return render_template("login.html", form=login_form)
+
+@app.route('/logout')
+def logout():
+    if session.get('user'):
+        session.clear()
+
+    return redirect(url_for('index'))
+
+@app.route('/posts/<post_id>/comment', methods=['POST'])
+def new_comment(post_id):
+    if session.get('user'):
+        comment_form = CommentForm()
+        if comment_form.validate_on_submit():
+            comment_text = request.form['comment']
+            new_record = Comment(comment_text, int(post_id), session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
+        return redirect(url_for('get_post', post_id=post_id))
+    else:
+        return redirect(url_for('login'))
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
--- appCode/models.py
+++ appCode/models.py
@@ -1,19 +1,45 @@
 from database import db
+import datetime
 
 class Post(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     title = db.Column("title", db.String(200))
     text = db.Column("text", db.String(100))
     date = db.Column("date", db.String(50))
-
-    def __init__(self, title, text, date):
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    comments = db.relationship("Comment", backref="note", cascade="all, delete-orphan", lazy=True)
+    def __init__(self, title, text, date, user_id):
         self.title = title
         self.text = text
         self.date = date
+        self.user_id = user_id
+
 class User(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("name", db.String(100))
+    first_name = db.Column("first_name", db.String(100))
+    last_name = db.Column("last_name", db.String(100))
     email = db.Column("email", db.String(100))
-    def __init__(self, name, email):
-       self.name = name
-       self.email = email
+    password = db.Column(db.String(255), nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    posts = db.relationship("Post", backref="user", lazy=True)
+    comments = db.relationship("Comment", backref="user", lazy=True)
+
+    def __init__(self, first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.date.today()
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False)
+    content = db.Column(db.VARCHAR, nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, content, post_id, user_id):
+        self.date_posted = datetime.date.today()
+        self.content = content
+        self.post_id = post_id
+        self.user_id = user_id
